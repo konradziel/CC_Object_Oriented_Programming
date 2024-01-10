64b0b9df8a242cb5f3689f691cef5ba0a10fe9de
Konrad Zieliński <fresher002@gmail.com>
2024-01-10 16:04:50 +0100

    Zadanie na aktywność - wariant 461

---
+++ _10.01.2024/src/ZadanieNaAktywność/Wariant461/w461.java
@@ -0,0 +1,24 @@
+package ZadanieNaAktywność.Wariant461;
+
+import java.util.ArrayList;
+
+public class w461 {
+    public static void main(String[] args) {
+        Integer[] tabInteger = {1, 3, -4, 9, 5, -6};
+        String[] tabString = {"abc", "def", "ghi", "jkl"};
+
+        System.out.println(contains(tabInteger, 3));
+        System.out.println(contains(tabInteger, "abc"));
+        System.out.println(contains(tabString, "abc"));
+        System.out.println(contains(tabString, 3));
+    }
+
+    public static <T> boolean contains(T[] tab, T x){
+        for(var elem: tab){
+            if(elem.equals(x)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
