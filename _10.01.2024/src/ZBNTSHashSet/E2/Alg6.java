307cb7ef2c61385e8a0d5a02093d6413639d56cc
Konrad Zieliński <fresher002@gmail.com>
2024-01-14 19:57:16 +0100

    E2 - poprawienie błędu w logice

--- _10.01.2024/src/ZBNTSHashSet/E2/Alg6.java
+++ _10.01.2024/src/ZBNTSHashSet/E2/Alg6.java
@@ -5,7 +5,7 @@ import java.util.HashSet;
 public class Alg6 {
     public <T> boolean hasCommonElements(HashSet<T> set1, HashSet<T> set2) {
         for(var elem: set1) {
-            if(set2.contains(set1)){
+            if(set2.contains(elem)){
                 return true;
             }
         }
